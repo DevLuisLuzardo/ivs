2c331b973db184f6842cb101bd5895825a8ced8e
devluisluzardo <devluisluzardo@gmail.com>
2024-09-11 01:43:21 +0000

    eleven commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,28 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../context/UserProvider";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const Navbar = () => {
-
     const { user, signOut, signIn } = useContext(UserContext);
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    const [showDropdown, setShowDropdown] = useState(false); // Estado para el menú desplegable
-
-    const openSidebar = () => {
-        setSidebarOpen(true);
-    };
-
-    const closeSidebar = () => {
-        setSidebarOpen(false);
-    };
-
-    const toggleDropdown = () => {
-        setShowDropdown(!showDropdown);
-    };
 
     return (
         <>
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light 
-            fixed-top py-3" >
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top py-3"
+            >
+                <div>
 
-                <NavLink to="/">
-                    <img src="/ivs_logo_3-1.png" alt="Logo" width="25px" />
-                </NavLink>
-                <span className="navbar-brand" style={{ textDecoration: 'none' }}>IVS</span>
-
-                <NavLink to="/" className="nav-link mx-2"></NavLink>
-
-                <div className="ms-auto">
-
-                    <a className="text-dark"
-                        href="#accederadevocionales"
-                        style={{ textDecoration: 'none', paddingRight: '15px' }}
-                    >
-                        Devocionales
-                    </a>
-
-
-
-                    {/* Botón para abrir la sidebar */}
-                    <button className="openbtn" onClick={openSidebar}>
-                        ☰
-                    </button>
+                    <NavLink to="/">
+                        <img src="/ivs_logo_3-1.png" alt="Logo" width="25px" />
+                    </NavLink>
+                    <span className="navbar-brand" style={{ textDecoration: 'none' }}>IVS</span>
+                    <NavLink to="/" className="nav-link mx-2"></NavLink>
                 </div>
 
+
             </nav>
-
-
-            {/* Sidebar (se muestra si sidebarOpen es true) */}
-            <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
-                <button className="closebtn" onClick={closeSidebar}>
-                    ×
-                </button>
-                <a href="#">Devocionales</a>
-                {/* Agrega más elementos de menú aquí */}
-            </div>
-
 
         </>
     );
